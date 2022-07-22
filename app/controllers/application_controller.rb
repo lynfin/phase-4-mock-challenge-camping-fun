d8bccbddbf4cacc3cffd8a15667e864acbace5a0
Lyn <lynfinman@gmail.com>
2022-07-22 17:48:17 -0500

    all tests pass

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :bad_entity_data
 
+  private
+
+  def record_not_found(error)
+    render json: { error: "#{error.model} not found" }, status: :not_found
+  end
+
+  def bad_entity_data(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end
 end
